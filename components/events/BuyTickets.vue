467b3613a918bf34ab859e9f79e7916e97cb2a5f
stephenprabhu <stephenmachado1998@gmail.com>
2024-07-20 08:05:39 +0400

    added buy tickets styles

--- components/events/BuyTickets.vue
+++ components/events/BuyTickets.vue
@@ -12,16 +12,16 @@
 .buybutton {
     display: flex;
     align-items: center;
-    justify-content: center; /* This centers the content horizontally */
-    padding: 10px 20px; /* Optional: Add padding for better appearance */
-    border: none; /* Optional: Remove border */
-    background-color: black; /* Optional: Add background color */
-    cursor: pointer; /* Optional: Change cursor to pointer on hover */
+    justify-content: center; 
+    padding: 10px 20px; 
+    border: none; 
+    background-color: black; 
+    cursor: pointer; 
     color: white;
     font-family: $font-stack;
 }
 
 .buybutton img {
-    margin-left: 0px; /* Add space between the text and the image */
+    margin-left: 0px; 
 }
 </style>
