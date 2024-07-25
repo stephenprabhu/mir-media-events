d5ce5b3feb9f421ee280dee6491a19ebecc964c1
stephenprabhu <stephenmachado1998@gmail.com>
2024-07-25 23:59:13 +0800

    added dark button changes

--- components/general/DarkButtton.vue
+++ components/general/DarkButtton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="buybutton" :aria-label="label">
+  <button class="darkbutton" :aria-label="label">
     <slot></slot>
   </button>
 </template>
@@ -10,7 +10,7 @@
 </script>
 
 <style scoped lang="scss">
-  .buybutton {
+  .darkbutton {
         @include dark-box;
         display: flex;
         align-items: center;
